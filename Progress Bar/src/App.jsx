f57e7117902535235c1fde2c5e1eef68f8bc5e66
Piyushagrawal1 <agarwalpiyush166@gmail.com>
2024-02-26 15:20:45 +0530

    Progress Bar complete code

---
+++ Progress Bar/src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import ProgressBar from "./components/ProgressBar"
+
+const App = () => {
+
+  const [value, setValue] = useState(0)
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    setInterval(() => {
+      setValue((val) => val + 1)
+    }, 100);
+  }, [])
+
+  return (
+    <div className='app'>
+      <span>Progress Bar</span>
+      <ProgressBar
+        value={value}
+        onComplete={() => setSuccess(true)}
+      />
+      <span>{success ? 'Complete!' : 'Loading...'}</span>
+    </div>
+  )
+}
+
+
+export default App
